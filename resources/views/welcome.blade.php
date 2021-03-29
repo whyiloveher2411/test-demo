97665d8054d2912ef786ca6a428323357488e3c7
whyiloveher2411 <quan.dang@dna.vn>
2021-03-29 16:19:10 +0700

    Your message about the commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" type="text/css" href="{{mix('css/app.css')}}">
+    </head>
+    <body>
+        
+        <div id="example"></div>
+        <script type="text/javascript" src="{{mix('js/app.js')}}"></script>
+    </body>
+</html>
